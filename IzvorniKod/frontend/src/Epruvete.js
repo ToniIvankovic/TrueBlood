17176b9037ed429b71131c377b344b7d9f456091
Borna <bm52640@fer.hr>
2021-12-17 19:23:52 +0100

    homepage-zalihekrvi

---
+++ IzvorniKod/frontend/src/Epruvete.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Epruvete = ({done}) => {
+        const [style, setStyle] = React.useState({});
+        
+        setTimeout(() => {
+            const newStyle = {
+                opacity: 1,
+                width: `${done}%`
+            }
+            
+            setStyle(newStyle);
+        }, 200);
+        
+        return (
+            <div className="progress">
+                <div className="progress-done" style={style}></div>
+            </div>
+        )
+}
+export default Epruvete;
