9083d60231fe09397554ddcd839ff1a2988d4621
ToniIvankovic <toni.ivankovic@fer.hr>
2021-12-19 20:52:54 +0100

    Skriveni podatci dok nije odabran donor

--- IzvorniKod/frontend/src/PokusajDoniranja.js
+++ IzvorniKod/frontend/src/PokusajDoniranja.js
@@ -43,82 +43,87 @@ const PokusajDoniranja = (props) => {
                     :''
                     }
                 </div>
-                <div className="label">
-                    <label>Osobni podaci</label>
-                </div>
-                <div className="single">
-                    <input
-                        name='donorId'
-                        type="text"
-                        placeholder={"donorId: " + props.donor.donorId}
-                        ></input>
-                </div>
-                <div className="dupli">
-                    <input
-                        name='firstName'
-                        type="text"
-                        placeholder={"ime: " + props.donor.firstName}
-                        ></input>
-                    <input
-                        name='lastName'
-                        type="text"
-                        placeholder={"prezime: " + props.donor.lastName}
-                        ></input>
-                </div>
-                <div className="single">
-                    <input
-                        name='oib'
-                        type="text"
-                        placeholder={"OIB: " + props.donor.oib}
-                        ></input>
-                </div>
-                <div className="krgrupe">
-                    <label>Krvna grupa</label>
-                    <select value={props.donor.bloodType}>
-                        <option value="---">---</option>
-                        <option value="A+">A+</option>
-                        <option value="A-">A-</option>
-                        <option value="B+">B+</option>
-                        <option value="B-">B-</option>
-                        <option value="AB+">AB+</option>
-                        <option value="AB-">AB-</option>
-                        <option value="0+">0+</option>
-                        <option value="0-">0-</option>
-                    </select>
-                </div>
+                {props.existingDonor? 
+                <div>
+                    <div className="label">
+                        <label>Osobni podaci</label>
+                    </div>
+                    <div className="single">
+                        <input
+                            name='donorId'
+                            type="text"
+                            placeholder={"donorId: " + props.donor.donorId}
+                            ></input>
+                    </div>
+                    <div className="dupli">
+                        <input
+                            name='firstName'
+                            type="text"
+                            placeholder={"ime: " + props.donor.firstName}
+                            ></input>
+                        <input
+                            name='lastName'
+                            type="text"
+                            placeholder={"prezime: " + props.donor.lastName}
+                            ></input>
+                    </div>
+                    <div className="single">
+                        <input
+                            name='oib'
+                            type="text"
+                            placeholder={"OIB: " + props.donor.oib}
+                            ></input>
+                    </div>
+                    <div className="krgrupe">
+                        <label>Krvna grupa</label>
+                        <select value={props.donor.bloodType}>
+                            <option value="---">---</option>
+                            <option value="A+">A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="0+">0+</option>
+                            <option value="0-">0-</option>
+                        </select>
+                    </div>
 
-                <div className="label">
-                    <label>Zdravstveni podaci</label>
+                    <div className="label">
+                        <label>Zdravstveni podaci</label>
+                    </div>
+                    
+                    <ZdravstveniPodaci question={"Tjelesna masa ispod 55 kg?"}/>
+                    <ZdravstveniPodaci question={"Tjelesna temperatura iznad 37°C?"}/>
+                    <ZdravstveniPodaci question={"Povišen ili nizak krvni tlak?"}/>
+                    <ZdravstveniPodaci question={"Osoba ima ubrzan rad srca?"}/>
+                    <ZdravstveniPodaci question={"Osoba ima povišenu razinu hemoglobina u krvi?"}/>
+                    <ZdravstveniPodaci question={"Osoba trenutno uzima antibiotike ili druge lijekove?"}/>
+                    <ZdravstveniPodaci question={"Osoba je konzumirala alkoholna pića unutar 8 sati prije darivanja krvi?"}/>
+                    <ZdravstveniPodaci question={"Osoba boluje od lakše aktune bolesti?"}/>
+                    <ZdravstveniPodaci question={"Osoba (žena) je trudna, doji ili ima menstruaciju?"}/>
+                    <ZdravstveniPodaci question={"Osoba tog dana obavlja opasne poslove (visinski / dubinski radovi)?"}/>
+
+                    <hr className='label' />
+
+                    <ZdravstveniPodaci question={"Osoba je bolovala ili boluje od teških kroničnih bolesti dišnog i/ili probavnog sustava?"}/>
+                    <ZdravstveniPodaci question={"Osoba boluje od bolesti srca i krvnih žila, zloćudnih bolesti, bolesti jetre, AIDS-a, šećerne bolesti te živčanih i duševnih bolesti?"}/>
+                    <ZdravstveniPodaci question={"Osoba je ovisnik o alkoholu ili drogama?"}/>
+                    <ZdravstveniPodaci question={"Osoba (muškarac) je u životu imala spolni odnos s drugim muškarcima?"}/>
+                    <ZdravstveniPodaci question={"Osoba je imala spolni odnos s prostitutkama?"}/>
+                    <ZdravstveniPodaci question={"Osoba često mijenja seksualne partnere (promiskuitetna osoba)?"}/>
+                    <ZdravstveniPodaci question={"Osoba je uzimala drogu intravenskim putem?"}/>
+                    <ZdravstveniPodaci question={"Osoba je liječana od spolno prenosivih bolesti?"}/>
+                    <ZdravstveniPodaci question={"Osoba je HIV-pozitivna?"}/>
+                    <ZdravstveniPodaci question={"Osoba je seksualni partner gore navedenih osoba?"}/>
+
+                    {errorHidden ? null : <ErrorCard message={errorMessage} />}
+                    <div className="gumbi">
+                        <button className='kreiraj'>Doniraj</button>
+                    </div>
                 </div>
+                :''}
                 
-                <ZdravstveniPodaci question={"Tjelesna masa ispod 55 kg?"}/>
-                <ZdravstveniPodaci question={"Tjelesna temperatura iznad 37°C?"}/>
-                <ZdravstveniPodaci question={"Povišen ili nizak krvni tlak?"}/>
-                <ZdravstveniPodaci question={"Osoba ima ubrzan rad srca?"}/>
-                <ZdravstveniPodaci question={"Osoba ima povišenu razinu hemoglobina u krvi?"}/>
-                <ZdravstveniPodaci question={"Osoba trenutno uzima antibiotike ili druge lijekove?"}/>
-                <ZdravstveniPodaci question={"Osoba je konzumirala alkoholna pića unutar 8 sati prije darivanja krvi?"}/>
-                <ZdravstveniPodaci question={"Osoba boluje od lakše aktune bolesti?"}/>
-                <ZdravstveniPodaci question={"Osoba (žena) je trudna, doji ili ima menstruaciju?"}/>
-                <ZdravstveniPodaci question={"Osoba tog dana obavlja opasne poslove (visinski / dubinski radovi)?"}/>
-
-                <hr className='label' />
-
-                <ZdravstveniPodaci question={"Osoba je bolovala ili boluje od teških kroničnih bolesti dišnog i/ili probavnog sustava?"}/>
-                <ZdravstveniPodaci question={"Osoba boluje od bolesti srca i krvnih žila, zloćudnih bolesti, bolesti jetre, AIDS-a, šećerne bolesti te živčanih i duševnih bolesti?"}/>
-                <ZdravstveniPodaci question={"Osoba je ovisnik o alkoholu ili drogama?"}/>
-                <ZdravstveniPodaci question={"Osoba (muškarac) je u životu imala spolni odnos s drugim muškarcima?"}/>
-                <ZdravstveniPodaci question={"Osoba je imala spolni odnos s prostitutkama?"}/>
-                <ZdravstveniPodaci question={"Osoba često mijenja seksualne partnere (promiskuitetna osoba)?"}/>
-                <ZdravstveniPodaci question={"Osoba je uzimala drogu intravenskim putem?"}/>
-                <ZdravstveniPodaci question={"Osoba je liječana od spolno prenosivih bolesti?"}/>
-                <ZdravstveniPodaci question={"Osoba je HIV-pozitivna?"}/>
-                <ZdravstveniPodaci question={"Osoba je seksualni partner gore navedenih osoba?"}/>
-
-                {errorHidden ? null : <ErrorCard message={errorMessage} />}
-                <div className="gumbi">
-                    <button className='kreiraj'>Doniraj</button>
-                </div>
             </form>
         </div>
     )
