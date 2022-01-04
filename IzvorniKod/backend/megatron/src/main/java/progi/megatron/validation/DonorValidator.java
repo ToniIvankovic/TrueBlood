afa0952792d904765cb4bb00dee375af2e85274e
Ana <ana.grskovic56@gmail.com>
2022-01-04 13:58:27 +0100

    Added checking donor's and bankworker's age.

--- IzvorniKod/backend/megatron/src/main/java/progi/megatron/validation/DonorValidator.java
+++ IzvorniKod/backend/megatron/src/main/java/progi/megatron/validation/DonorValidator.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 import progi.megatron.exception.WrongDonorException;
 import progi.megatron.model.Donor;
 
+import java.time.LocalDate;
+
 @Component
 public class DonorValidator {
 
@@ -18,6 +20,7 @@ public class DonorValidator {
     public boolean validateDonor(Donor donor) {
         if (oibValidator.validateOib(donor.getOib()) == false) return false;
         if (!donor.getEmail().contains("@")) throw new WrongDonorException("Donor email is not correct. ");
+        if (donor.getBirthDate().plusYears(18).isAfter(LocalDate.now())) throw new WrongDonorException("Donor must be at least 18 years old. ");
         if (donor.getBloodType() != null) {
             bloodSUpplyValidator.validateBloodType(donor.getBloodType());
         }
